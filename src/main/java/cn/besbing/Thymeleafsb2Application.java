39e1fbbdf6088023dac888e93b733bb819f7a73b
sheny <fsbydz@vip.qq.com>
2019-11-01 00:04:25 +0800

    Initial commit

---
+++ src/main/java/cn/besbing/Thymeleafsb2Application.java
@@ -0,0 +1,13 @@
+package cn.besbing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thymeleafsb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Thymeleafsb2Application.class, args);
+    }
+
+}
