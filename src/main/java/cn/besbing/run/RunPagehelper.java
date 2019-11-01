8114d54e2dd3b993e171a4a5fd36dcceefa12302
sheny <fsbydz@vip.qq.com>
2019-11-01 16:43:45 +0800

    工具类：mabatis逆向工程增加ls_user_info表逆向工程

---
+++ src/main/java/cn/besbing/run/RunPagehelper.java
@@ -0,0 +1,17 @@
+package cn.besbing.run;
+
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class RunPagehelper {
+
+    public static void main(String[] args) {
+
+    }
+}
